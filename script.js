d3718dad043c1207246f5a04d95d66a53de9c9e9
asanov-rasul <156917189+asanov-rasul@users.noreply.github.com>
2024-06-12 15:22:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -53,55 +53,52 @@ async function handleInput(event) {
       setupInputOnce();
       return;
   }
+}
 
-  document.addEventListener('touchstart', function (e) {
-            startX = e.touches[0].clientX;
-            startY = e.touches[0].clientY;
-        });
+document.addEventListener('touchstart', function (e) {
+  startX = e.touches[0].clientX;
+  startY = e.touches[0].clientY;
+});
 
-        document.addEventListener('touchend', function (e) {
-            var endX = e.changedTouches[0].clientX;
-            var endY = e.changedTouches[0].clientY;
-            var deltaX = startX - endX;
-            var deltaY = startY - endY;
-            var minSwipeLength = 20;
+document.addEventListener('touchend', function (e) {
+  var endX = e.changedTouches[0].clientX;
+  var endY = e.changedTouches[0].clientY;
+  var deltaX = startX - endX;
+  var deltaY = startY - endY;
+  var minSwipeLength = 20;
 
-            if (Math.abs(deltaX) > minSwipeLength || Math.abs(deltaY) > minSwipeLength) {
-                if (Math.abs(deltaX) > Math.abs(deltaY)) {
-                    if (deltaX > 0) {
-                        if (!canMoveLeft()) {
-                          setupInputOnce();
-                          return;
-                        }
-                        await moveLeft();
-                        break;
-                    } else {
-                        if (!canMoveRight()) {
-                          setupInputOnce();
-                          return;
-                        }
-                        await moveRight();
-                        break;
-                    }
-                } else {
-                    if (deltaY > 0) {
-                        if (!canMoveUp()) {
-                          setupInputOnce();
-                          return;
-                        }
-                        await moveUp();
-                        break;
-                    } else {
-                        if (!canMoveDown()) {
-                          setupInputOnce();
-                          return;
-                        }
-                        await moveDown();
-                        break;
-                    }
-                }
-            }
-        });
+  if (Math.abs(deltaX) > minSwipeLength || Math.abs(deltaY) > minSwipeLength) {
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      if (deltaX > 0) {
+        if (!canMoveLeft()) {
+          setupInputOnce();
+          return;
+        }
+        moveLeft();
+      } else {
+        if (!canMoveRight()) {
+          setupInputOnce();
+          return;
+        }
+        moveRight();
+      }
+    } else {
+      if (deltaY > 0) {
+        if (!canMoveUp()) {
+          setupInputOnce();
+          return;
+        }
+        moveUp();
+      } else {
+        if (!canMoveDown()) {
+          setupInputOnce();
+          return;
+        }
+        moveDown();
+      }
+    }
+  }
+});
 
   
   const newTile = new Tile(gameBoard);
