fd7c70ee7b2560cfd7fe12bf3226a522052377d1
asanov-rasul <156917189+asanov-rasul@users.noreply.github.com>
2024-06-12 15:10:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { Grid } from "./grid.js";
 import { Tile } from "./tile.js";
 
 const gameBoard = document.getElementById("game-board");
+var startX, startY;
 
 const grid = new Grid(gameBoard);
 grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
@@ -51,6 +52,56 @@ async function handleInput(event) {
     default:
       setupInputOnce();
       return;
+
+      
+       document.addEventListener('touchstart', function (e) {
+            startX = e.touches[0].clientX;
+            startY = e.touches[0].clientY;
+        });
+
+        document.addEventListener('touchend', function (e) {
+            var endX = e.changedTouches[0].clientX;
+            var endY = e.changedTouches[0].clientY;
+            var deltaX = startX - endX;
+            var deltaY = startY - endY;
+            var minSwipeLength = 20;
+
+            if (Math.abs(deltaX) > minSwipeLength || Math.abs(deltaY) > minSwipeLength) {
+                if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                    if (deltaX > 0) {
+                        if (!canMoveLeft()) {
+                          setupInputOnce();
+                          return;
+                        }
+                        await moveLeft();
+                        break;
+                    } else {
+                        if (!canMoveRight()) {
+                          setupInputOnce();
+                          return;
+                        }
+                        await moveRight();
+                        break;
+                    }
+                } else {
+                    if (deltaY > 0) {
+                        if (!canMoveUp()) {
+                          setupInputOnce();
+                          return;
+                        }
+                        await moveUp();
+                        break;
+                    } else {
+                        if (!canMoveDown()) {
+                          setupInputOnce();
+                          return;
+                        }
+                        await moveDown();
+                        break;
+                    }
+                }
+            }
+        });
   }
 
   const newTile = new Tile(gameBoard);
